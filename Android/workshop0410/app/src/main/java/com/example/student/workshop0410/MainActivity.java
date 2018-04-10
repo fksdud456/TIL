7216bcae9b0261882cea1479098fdaef99895d57
M140320\student <fksdud456@gmail.com>
2018-04-10 17:50:18 +0900

    Update UI ( Add home, login, register,  mv project layout )
    - mv 프로젝트 서버가 실행중이어야 분석 화면이 실행된다
    - Update AndroidManifest.xml  WebView 사용을 위한 설정
    -
    2018/04/10

--- Android/workshop0410/app/src/main/java/com/example/student/workshop0410/MainActivity.java
+++ Android/workshop0410/app/src/main/java/com/example/student/workshop0410/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.student.workshop0410;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_home;
+    TextView tx_time, login_id, login_pwd;
+    EditText login_tx_id, login_tx_pwd;
+    LinearLayout l_login, l_home, l_register;
+    WebView wv_mv, wv_blog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeUI();
+    }
+
+    public void makeUI() {
+        tx_time         = findViewById(R.id.tx_time);
+        login_id        = findViewById(R.id.login_id);
+        login_pwd       = findViewById(R.id.login_pwd);
+        login_tx_id     = findViewById(R.id.login_tx_id);
+        login_tx_pwd    = findViewById(R.id.login_tx_pwd);
+        l_login         = findViewById(R.id.l_login);
+        l_home          = findViewById(R.id.l_home);
+        l_register      = findViewById(R.id.l_register);
+        wv_mv           = findViewById(R.id.wv_mv);
+        wv_blog         = findViewById(R.id.wv_blog);
+
+        // webview setting
+        wv_mv.setWebViewClient(new WebViewClient());
+        wv_mv.getSettings().setJavaScriptEnabled(true);
+
+        wv_blog.setWebViewClient(new WebViewClient());
+        wv_blog.getSettings().setJavaScriptEnabled(true);
+        wv_blog.loadUrl("https://yumdevelop.blogspot.com");
+
+        l_home.setVisibility(View.VISIBLE);
+        l_login.setVisibility(View.INVISIBLE);
+        l_register.setVisibility(View.INVISIBLE);
+        wv_mv.setVisibility(View.INVISIBLE);
+    }
+
+    public void onClickBt(View v) {
+        if(v.getId() == R.id.bt_home) {
+            l_login.setVisibility(View.INVISIBLE);
+            l_home.setVisibility(View.VISIBLE);
+            l_register.setVisibility(View.INVISIBLE);
+            wv_mv.setVisibility(View.INVISIBLE);
+        } else if(v.getId() == R.id.bt_login) {
+            l_login.setVisibility(View.VISIBLE);
+            l_home.setVisibility(View.INVISIBLE);
+            l_register.setVisibility(View.INVISIBLE);
+            wv_mv.setVisibility(View.INVISIBLE);
+        } else if(v.getId() == R.id.bt_register) {
+            l_login.setVisibility(View.INVISIBLE);
+            l_home.setVisibility(View.INVISIBLE);
+            l_register.setVisibility(View.VISIBLE);
+            wv_mv.setVisibility(View.INVISIBLE);
+        } else if(v.getId() == R.id.bt_analysis) {
+            l_login.setVisibility(View.INVISIBLE);
+            l_home.setVisibility(View.INVISIBLE);
+            l_register.setVisibility(View.INVISIBLE);
+
+            wv_mv.loadUrl("http://70.12.114.150:80/mv/");
+            wv_mv.setVisibility(View.VISIBLE);
+        }
     }
 }
