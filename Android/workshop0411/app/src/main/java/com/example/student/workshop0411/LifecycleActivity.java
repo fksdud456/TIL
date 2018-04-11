fe12a26641a2f535ed45c758ae7ef863b360b1bc
M140320\student <fksdud456@gmail.com>
2018-04-11 19:14:57 +0900

    Update Lifecycle Activity
    Add buttons into Main Activity
    Add using Intent example
    If you want to use Intent.ACTION_CALL , you have to get permission
    > add permission tag into AndroidManifest.xml
    
    2018/04/11

--- Android/workshop0411/app/src/main/java/com/example/student/workshop0411/LifecycleActivity.java
+++ Android/workshop0411/app/src/main/java/com/example/student/workshop0411/LifecycleActivity.java
@@ -1,7 +1,10 @@
 package com.example.student.workshop0411;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LifecycleActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class LifecycleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle);
+    }
+
+    public void clickBt(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.button:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+                break;
+            case R.id.button2:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-2659-0000"));
+                break;
+            case R.id.button3:
+                intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-2659-0000"));
+                break;
+        }
+        startActivity(intent);
     }
 }
--- Android/workshop0411/app/src/main/java/com/example/student/workshop0411/MainActivity.java
+++ Android/workshop0411/app/src/main/java/com/example/student/workshop0411/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.workshop0411;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickBt(View v) {
+        Intent intent = null;
+        if(v.getId() == R.id.bt_lifecycle) {
+            intent = new Intent(getApplicationContext(), LifecycleActivity.class);
+        }
+
+        startActivity(intent);
     }
 }
