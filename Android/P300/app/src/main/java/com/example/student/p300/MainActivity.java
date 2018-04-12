f88bc570e4c7653a6351f3361671aeae71e41e15
M140320\student <fksdud456@gmail.com>
2018-04-12 15:44:20 +0900

    Add onBackPressed() method, Progress DIalog
    2018/04/22

--- Android/P300/app/src/main/java/com/example/student/p300/MainActivity.java
+++ Android/P300/app/src/main/java/com/example/student/p300/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.student.p300;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
     ImageView imageView;
 
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         tx_service = findViewById(R.id.tx_service);
         progressBar= findViewById(R.id.progressBar);
         imageView = findViewById(R.id.imageView);
+
+        // MainActivity 에서 띄우기 위해서 MainActivity.this 로 생성
+        // getApplicationContext()
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false);
     }
 
     @Override
@@ -56,6 +65,48 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("name", name);
         // startActivity 아니고 startService
         startService(intent);
+    }
+
+    public void clickBt2(View v) {
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("진행중");
+        progressDialog.show();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("경고");
+        builder.setMessage("끝내시겠습니까?");
+        builder.setIcon(R.mipmap.icon1);
+        builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                return;
+            }
+        });
+
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                return;
+            }
+        });
+
+        builder.show();
     }
 
     @Override
