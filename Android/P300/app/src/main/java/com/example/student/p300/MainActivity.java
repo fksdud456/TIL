8f8fcfc71cef1697115a2331833a172e97983f50
M140320\student <fksdud456@gmail.com>
2018-04-12 14:25:10 +0900

    Create P300 project
    How to use service
     > extends Service class
     > use startService() in MainActivity
    If you click the button, service started with new thread
    2018/04/12

---
+++ Android/P300/app/src/main/java/com/example/student/p300/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.student.p300;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView tx_service;
+    Intent intent;
+    ProgressBar progressBar;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        tx_service = findViewById(R.id.tx_service);
+        progressBar= findViewById(R.id.progressBar);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        processIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+        if (intent != null) {
+            String command = intent.getStringExtra("command");
+            int cnt = intent.getIntExtra("cnt", 0);
+            tx_service.setText(command + " " + cnt);
+            progressBar.setProgress(cnt*10);
+            if(cnt%2 == 0) {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.bg4));
+            } else {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.bg0));
+            }
+        }
+    }
+
+    public void clickBt(View v) {
+        String name = editText.getText().toString();
+        // 암시적인 intent, 명시적인 Intent를 통해
+        //어떤 activity나 객체를 호출할 때 사용한다.
+        // 값을 전달할 때도 사용
+        intent = new Intent(this, MyService.class);
+        intent.putExtra("command", "show");
+        intent.putExtra("name", name);
+        // startActivity 아니고 startService
+        startService(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (intent != null) {
+            stopService(intent);
+        }
+    }
+}
