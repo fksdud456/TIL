3b523aa248b041aa4727a1389efacf9ff7a985e7
M140320\student <fksdud456@gmail.com>
2018-04-12 10:16:20 +0900

    Create MyFragment project
    2018/04/12

---
+++ Android/MyFragment/app/src/main/java/com/example/student/myfragment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.student.myfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            /*getSupportFragmentManager()*/
+
+        }
+    }
+}
