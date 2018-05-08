3b25258c2f6c110d3fd44e7b6f01f7eff51d430e
M140320\student <fksdud456@gmail.com>
2018-05-08 14:12:21 +0900

    Add Receive and Send function

--- Network/day05/src/s1/SerialTest.java
+++ Network/day05/src/s1/SerialTest.java
@@ -1,30 +1,155 @@
 package s1;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.NoSuchPortException;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
 
-public class SerialTest {
-	CommPortIdentifier commPortIdentifier;
-	
+public class SerialTest implements SerialPortEventListener {
+	// CommPortIdentifier commPortIdentifier;
+
+	private BufferedInputStream bin;
+	private InputStream in;
+	private OutputStream out;
+	private SerialPort serialPort;
+	private CommPortIdentifier portIdentifier;
+	private CommPort commPort;
+
 	public SerialTest() {
-		
 	}
-	
+
 	public SerialTest(String portName) throws NoSuchPortException {
-		commPortIdentifier = 
-				CommPortIdentifier.getPortIdentifier(portName);
-		System.out.println("Connect Com Port!");
+		portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		System.out.println("Connect Com Port");
+		try {
+			connectSerial();
+			System.out.println("Connect OK !!");
+			(new Thread(new SerialWriter())).start();
+		} catch (Exception e) {
+			System.out.println("Connect Fail !!");
+			e.printStackTrace();
+		}
+	}
+
+	public void connectSerial() throws Exception {
+
+		if (portIdentifier.isCurrentlyOwned()) {
+			System.out.println("Error: Port is currently in use");
+		} else {
+			commPort = portIdentifier.open(this.getClass().getName(), 5000);
+			if (commPort instanceof SerialPort) {
+				serialPort = (SerialPort) commPort;
+				serialPort.addEventListener(this);
+				serialPort.notifyOnDataAvailable(true);
+				serialPort.setSerialPortParams(921600, // 통신속도
+						SerialPort.DATABITS_8, // 데이터 비트
+						SerialPort.STOPBITS_1, // stop 비트
+						SerialPort.PARITY_NONE); // 패리티
+				in = serialPort.getInputStream();
+				bin = new BufferedInputStream(in);
+				out = serialPort.getOutputStream();
+			} else {
+				System.out.println("Error: Only serial ports are handled by this example.");
+			}
+		}
 	}
 	
+	// 보내는 역할
+	public void sendMsg(String msg) {
+		// :W28 00000000 0000000000000000 53 \r	
+		SerialWriter sw = new SerialWriter(msg);
+		new Thread(sw).start();
+	}
+	
+	private class SerialWriter implements Runnable {
+		String data;
+
+		public SerialWriter() {
+			this.data = ":G11A9\r";
+		}
+
+		public SerialWriter(String serialData) {
+			this.data = sendDataFormat(serialData);
+		}
+
+		public String sendDataFormat(String serialData) {
+			serialData = serialData.toUpperCase();
+			char c[] = serialData.toCharArray();
+			int cdata = 0;
+			for (char cc : c) {
+				cdata += cc;
+			}
+			// checksum data 만드는 과정
+			cdata = (cdata & 0xFF);
+
+			String returnData = ":";
+			returnData += serialData + Integer.toHexString(cdata).toUpperCase();
+			returnData += "\r";
+			return returnData;
+		}
+
+		public void run() {
+			try {
+
+				byte[] inputData = data.getBytes();
+
+				out.write(inputData);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		SerialTest serialTest = null;
+
 		try {
-			serialTest = new SerialTest("COM13");
+			serialTest = new SerialTest("COM14");
 		} catch (NoSuchPortException e) {
-			System.out.println("Connect Fail!");
+			System.out.println("connect Fail !");
 			e.printStackTrace();
-		}		
+		}
+
+	}
+
+	@Override
+	public void serialEvent(SerialPortEvent event) {
+		switch (event.getEventType()) {
+		case SerialPortEvent.BI:
+		case SerialPortEvent.OE:
+		case SerialPortEvent.FE:
+		case SerialPortEvent.PE:
+		case SerialPortEvent.CD:
+		case SerialPortEvent.CTS:
+		case SerialPortEvent.DSR:
+		case SerialPortEvent.RI:
+		case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+			break;
+		case SerialPortEvent.DATA_AVAILABLE:
+			byte[] readBuffer = new byte[128];
+
+			try {
+
+				while (bin.available() > 0) {
+					int numBytes = bin.read(readBuffer);
+				}
+
+				String ss = new String(readBuffer);
+				System.out.println("Receive Low Data:" + ss + "||");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+		}
 	}
 
 }
